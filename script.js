2f10cbee26266ac8ed774592ff02bcbedce65a85
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-20 11:57:13 +1100

    v1 complete

--- script.js
+++ script.js
@@ -14,52 +14,77 @@ const fileStrucutre = [
                 folders: {}
             }
         ]
-    }
+    },
+    {
+        id: 1,
+        name: "Week 3 PQMS",
+        folders: [
+            {
+                id: 2,
+                name: "Tutorial 2",
+                folders: {}
+            },
+            {
+                id: 2,
+                name: "Tutorial 4",
+                folders: {}
+            }
+        ]
+    },
 ]
 
 const container = document.querySelector('.container')
-const fileBtn = document.querySelector(".file")
-const folderBtn = document.querySelector(".folder")
 
 
 window.addEventListener("load", (e) => {
-
+    
     fileStrucutre.forEach(files => {
-
+        
         const fileList = document.createElement('div')
         fileList.classList.add('files')
         const fileButton = document.createElement('button')
         fileButton.classList.add('file')
         fileButton.innerHTML = files.name
- 
+        
         fileList.appendChild(fileButton)
         container.appendChild(fileList)
 
+
+        
         console.log(fileList);
         console.log(fileButton);
-
+        
         if(Object.keys(fileStrucutre).length !== 0){
             
             const folders = files.folders
 
+           
+            
             folders.forEach(folder => {
                 const folderList = document.createElement('div')
                 folderList.classList.add('folders')
-    
+                
                 const folderButton = document.createElement('button')
                 folderButton.classList.add('folder')
                 
                 folderButton.innerHTML = folder.name
-    
+
+                  folderList.style.display = 'none'
+                
                 folderList.appendChild(folderButton)
                 fileList.appendChild(folderList)
 
+                fileButton.addEventListener("click", (e) => {
+                    folderList.style.display = folderList.style.display === 'none' ? 'flex' : 'none';
+                })
+                
             })
-
-
+            
+            
             
         }
     })
+    
 
 })
 
