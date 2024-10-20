486cf9ce3071e1b772c9c4ebfc1e2cde743cb606
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-20 11:46:23 +1100

    inital structure complete

--- script.js
+++ script.js
@@ -5,15 +5,69 @@ const fileStrucutre = [
         folders: [
             {
                 id: 2,
-                name: "Tutorial",
+                name: "Tutorial 1",
+                folders: {}
+            },
+            {
+                id: 2,
+                name: "Tutorial 2",
                 folders: {}
             }
         ]
     }
 ]
 
-
+const container = document.querySelector('.container')
 const fileBtn = document.querySelector(".file")
 const folderBtn = document.querySelector(".folder")
 
 
+window.addEventListener("load", (e) => {
+
+    fileStrucutre.forEach(files => {
+
+        const fileList = document.createElement('div')
+        fileList.classList.add('files')
+        const fileButton = document.createElement('button')
+        fileButton.classList.add('file')
+        fileButton.innerHTML = files.name
+ 
+        fileList.appendChild(fileButton)
+        container.appendChild(fileList)
+
+        console.log(fileList);
+        console.log(fileButton);
+
+        if(Object.keys(fileStrucutre).length !== 0){
+            
+            const folders = files.folders
+
+            folders.forEach(folder => {
+                const folderList = document.createElement('div')
+                folderList.classList.add('folders')
+    
+                const folderButton = document.createElement('button')
+                folderButton.classList.add('folder')
+                
+                folderButton.innerHTML = folder.name
+    
+                folderList.appendChild(folderButton)
+                fileList.appendChild(folderList)
+
+            })
+
+
+            
+        }
+    })
+
+})
+
+
+function getFolderName(folders){
+    folders.forEach(folder => {
+        console.log(folder.name);
+        
+        return folder.name
+    })
+}
