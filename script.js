9882b62fb2b1c698f176bc429768c0170c44555a
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-20 11:22:36 +1100

    styling done

---
+++ script.js
@@ -0,0 +1,19 @@
+const fileStrucutre = [
+    {
+        id: 1,
+        name: "Week 2 PQMS",
+        folders: [
+            {
+                id: 2,
+                name: "Tutorial",
+                folders: {}
+            }
+        ]
+    }
+]
+
+
+const fileBtn = document.querySelector(".file")
+const folderBtn = document.querySelector(".folder")
+
+
